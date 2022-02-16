8c30feedf0247ba4ff6145b0c5d6348706493f0a
Dawoud Mahmud <64564343+DawoudMahmud@users.noreply.github.com>
2022-02-15 19:34:03 -0800

    Formatted pictures and added captions

--- src/components/Phuket.vue
+++ src/components/Phuket.vue
@@ -1,15 +1,35 @@
 <template>
     <h1>Phuket</h1>
-
-<div class="pics">
-  <img src="../assets/Phuket/PaddleBoard.jpg">
-  <img src="../assets/Phuket/Buddha.jpg">
-</div>
-<div class="pics">
-  <img src="../assets/Phuket/cruise.jpg">
-  <img src="../assets/Phuket/Motorcycle.jpg">
-  <img src="../assets/Phuket/bangla.jpg">
-</div>
+      <div class = "parentFigureStyle">
+          <figure>
+            <img src="../assets/Phuket/PaddleBoard.jpg">
+            <figcaption class = "text">My first time paddleboarding</figcaption>
+          </figure>
+          <figure>
+            <img src="../assets/Phuket/Buddha.jpg">  
+            <figcaption class = "text">Visiting the Big Buddha</figcaption>
+          </figure>
+          <figure>
+            <img src="../assets/Phuket/cruise.jpg">
+            <figcaption class = "text">Having the time of our lives on a sunset cruise</figcaption>
+          </figure>
+      </div>
+      <div class="parentFigureStyle">
+        <figure>
+          <img src="../assets/Phuket/bangla.jpg">
+          <figcaption class = "text">Entrance to Bangla Road, the party street of Phuket</figcaption>
+        </figure>
+         <figure>
+          <img src="../assets/Phuket/Motorcycle.jpg">
+          <figcaption class = "text">Rented motorcycles to ride around the island</figcaption>
+        </figure>
+        <figure>
+          <img src="../assets/Phuket/sunset.jpg">
+          <figcaption class = "text">The most beautiful sunset I have ever seen</figcaption>
+        </figure>
+      </div>
+  
+  
 </template>
 
 <script>
@@ -24,8 +44,18 @@ export default {
     
 }
 .pics{
-  height: 800px;
-  width: 500px;
-  display: flex;
+  
+  padding: 50px
 }
+.parentFigureStyle {
+
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+.text {
+  font-size: 16pt;
+}
+
 </style>
